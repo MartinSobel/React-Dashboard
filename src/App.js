550526decdf917827fd06790e2b5df05751b8dab
MartinSobel <marto.sobel@gmail.com>
2021-02-11 16:50:36 -0300

    status

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import Sidebar from './components/Sidebar'
 import Topbar from './components/Topbar'
+import Status from './components/Status';
+
 import './style.css';
 import React from 'react'
 
@@ -11,8 +13,18 @@ function App() {
       <div id="content-wrapper" class="d-flex flex-column">
         <div id="content">
           <Topbar />
+
+          <div class="container-fluid">
+            <div class="d-sm-flex align-items-center justify-content-between mb-4">
+              <h1 class="h3 mb-0 text-gray-800">App Dashboard</h1>
+            </div>
+            <Status />
+
+          </div>
         </div>
       </div>
+
+
     </div>
   );
 }
