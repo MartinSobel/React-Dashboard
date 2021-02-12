8c779c0150c2d3115cbdbce0e10d0d8f4faac268
MartinSobel <marto.sobel@gmail.com>
2021-02-12 15:08:59 -0300

    colores e iconos

--- src/components/Status.js
+++ src/components/Status.js
@@ -4,21 +4,21 @@ import PropTypes from 'prop-types'
 function Status(props) {
     return (
         props.data.map((data) =>
-            <div class="col-md-4 mb-4">
-                <div class="card border-left-${data.color} shadow h-100 py-2">
-                    <div class="card-body">
-                        <div class="row no-gutters align-items-center">
-                            <div class="col mr-2">
-                                <div class="text-xs font-weight-bold text-${data.color} text-uppercase mb-1">{data.titulo}</div>
-                                <div class="h5 mb-0 font-weight-bold text-gray-800">{data.cifra}</div>
-                            </div>
-                            <div class="col-auto">
-                                <i class="fas ${data.icono} fa-2x text-gray-300"></i>
-                            </div>
+            <div className="col-md-4 mb-4">
+            <div className={`card border-left-${data.color} shadow h-100 py-2`}>
+                <div className="card-body">
+                    <div className="row no-gutters align-items-center">
+                        <div className="col mr-2">
+                            <div className={`text-xs font-weight-bold text-${data.color} text-uppercase mb-1`}>{data.titulo}</div>
+                            <div className="h5 mb-0 font-weight-bold text-gray-800">{data.cifra}</div>
+                        </div>
+                        <div className="col-auto">
+                            <i className={`fas ${data.icono} fa-2x text-gray-300`}></i>
                         </div>
                     </div>
                 </div>
             </div>
+        </div>
     ));
 }
 
